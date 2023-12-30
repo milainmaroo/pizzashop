3b1e6acf2ec61578ae7956385f60fa430e02361e
Mi Lainmar Oo <milainmar13@gmail.com>
2023-12-30 16:19:09 +1100

    Finalise Home Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/Home.css'
 import BackgroundImg from '../assets/pizza.jpeg'
 
@@ -6,9 +7,11 @@ function Home() {
   return (
     <div className='home' style={{ backgroundImage: `url(${BackgroundImg})` }}>
       <div className='home-container'>
-        <h1>Authentic Pizza</h1>
+        <h1>Authentic Pizzeria</h1>
         <p>PIZZA TO FIT ANY TASTE</p>
-        <button>ORDER NOW</button>
+        <Link to='/menu'>
+          <button>ORDER NOW</button>
+        </Link>
       </div>
     </div>
   )
