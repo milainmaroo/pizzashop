84e5d14db1de0e802219d30c74b181d5e900340f
Mi Lainmar Oo <milainmar13@gmail.com>
2023-12-30 16:55:57 +1100

    Create Footer Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' exact element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   )
