492a628691d81a43258d26fbcae28a48a9112975
Mi Lainmar Oo <milainmar13@gmail.com>
2023-12-30 21:56:02 +1100

    Create Contact Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from './pages/Contact'
 import Footer from './components/Footer'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' exact element={<Home />} />
           <Route path='/about' exact element={<About />} />
+          <Route path='/contact' exact element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Contact.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../styles/Contact.css'
+import PizzasImg from '../assets/pizzas.jpg'
+
+function Contact() {
+  return (
+    <div className='contact'>
+      <div
+        className='contact-img'
+        style={{ backgroundImage: `url(${PizzasImg})` }}
+      ></div>
+      <div className='contact-us'>
+        <h1>Contact Us</h1>
+        <form id='contact-form' method='POST'>
+          <label htmlFor='name'>Full Name</label>
+          <input type='text' name='name' placeholder='Enter full name...' />
+          <label htmlFor='email'>Email</label>
+          <input type='email' name='email' placeholder='Enter email...' />
+          <label htmlFor='message'>Message</label>
+          <textarea
+            name='message'
+            placeholder='Enter message...'
+            required
+            rows='6'
+          ></textarea>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
